cfdd377b3e78866155410b9203cb0948533b4915
nghts <n1ghtpromoso1@gmail.com>
2026-09-22 09:40:59 +0300

    commit test

---
+++ src/ru/mirea/task1/main.java
@@ -0,0 +1,9 @@
+package ru.mirea.task1;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+        System.out.println("Tester");
+        System.out.println("Third");
+        }
+    }
